25b4d3d2c496160f4665c8cf80f25dd57378b7eb
Hinko Kocevar <hinkocevar@gmail.com>
2018-03-09 17:54:41 +0100

    code refactoring
    added imgui

--- Makefile
+++ Makefile
@@ -1,39 +1,5 @@
-#TOPDIR  := $(shell cd ..; pwd)
-#include $(TOPDIR)/Rules.make
+all:
+	make -C src all
 
-DEPENDENCIES := -lpthread
-UNAME := $(shell uname)
-# Assume target is Mac OS if build host is Mac OS; any other host targets Linux
-ifeq ($(UNAME), Darwin)
-	DEPENDENCIES += -lobjc -framework IOKit -framework CoreFoundation
-else
-	DEPENDENCIES += -lrt
-endif
-
-CFLAGS = -Wall -Wextra
-CFLAGS += -ggdb3 -O0
-CFLAGS += -Ilibftd2xx
-
-DYNAMIC_LINK_OPTIONS := -L ./libftd2xx/build
-
-APP = simple
-STATIC_APP = $(APP)-static
-DYNAMIC_APP = $(APP)-dynamic
-
-#all: $(APP)-static $(APP)-dynamic 
-all: $(APP)-static
-
-imgecko.o: imgecko.cpp
-	g++ -c $< -o $@ $(CFLAGS) $(DEPENDENCIES)
-
-main.o: main.cpp
-	g++ -c $< -o $@ $(CFLAGS) $(DEPENDENCIES)
-
-$(STATIC_APP): imgecko.o main.o
-	g++ imgecko.o main.o -o $(STATIC_APP) ./libftd2xx/build/libftd2xx.a $(CFLAGS) $(DEPENDENCIES)
-
-#$(DYNAMIC_APP): main.c	
-#	$(CC) main.c -o $(DYNAMIC_APP) $(CFLAGS) -lftd2xx $(DEPENDENCIES) $(DYNAMIC_LINK_OPTIONS)
-	
 clean:
-	-rm -f *.o ; rm -f $(STATIC_APP); rm -f $(DYNAMIC_APP)
+	make -C src clean
---
+++ src/usbgecko.h
@@ -0,0 +1,79 @@
+/*
+ * usbgecko.h
+ *
+ *  Created on: Mar 7, 2018
+ *      Author: hinxx
+ */
+
+#ifndef USBGECKO_H_
+#define USBGECKO_H_
+
+#include <stdint.h>
+
+enum FTDICommand {
+	CMD_ResultError,
+	CMD_FatalError,
+	CMD_OK
+};
+
+enum WiiStatus {
+	WiiStatusRunning,
+	WiiStatusPaused,
+	WiiStatusBreakpoint,
+	WiiStatusLoader,
+	WiiStatusUnknown
+};
+
+enum WiiLanguage {
+	NoOverride,
+	Japanese,
+	English,
+	German,
+	French,
+	Spanish,
+	Italian,
+	Dutch,
+	ChineseSimplified,
+	ChineseTraditional,
+	Korean
+};
+
+enum WiiPatches {
+	NoPatches,
+	PAL60,
+	VIDTV,
+	PAL60VIDTV,
+	NTSC,
+	NTSCVIDTV,
+	PAL50,
+	PAL50VIDTV
+};
+
+enum WiiHookType {
+	VI,
+	WiiRemote,
+	GamecubePad
+};
+
+
+bool GeckoIsConnected();
+bool GeckoConnect();
+void GeckoDisconnect();
+bool GeckoReset();
+WiiStatus GeckoStatus();
+FTDICommand GeckoSendRawCommand(const uint8_t id);
+void GeckoSendFail();
+void GeckoSendRetry();
+void GeckoSendAck();
+bool GeckoDump(uint32_t startdump, uint32_t enddump, uint8_t *bytes);
+
+FTDICommand GeckoWrite(uint8_t *sendbyte, uint32_t nobytes);
+FTDICommand GeckoRead(uint8_t *recbyte, uint32_t nobytes);
+void GeckoPauseCmd();
+void GeckoSafePauseCmd();
+void GeckoResumeCmd();
+void GeckoSafeResumeCmd();
+
+
+
+#endif /* USBGECKO_H_ */
